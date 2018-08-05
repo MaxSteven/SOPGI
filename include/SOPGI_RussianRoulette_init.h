d4b329b0e8fcc7f34cb3d56e2daefa2d422bfa6c
Alessandro Nardini <42110232+alexnardini@users.noreply.github.com>
2018-08-05 06:42:00 -0700

    Update SOPGI_RussianRoulette_init.h

--- include/SOPGI_RussianRoulette_init.h
+++ include/SOPGI_RussianRoulette_init.h
@@ -1,13 +1,17 @@
+// https://graphics.stanford.edu/courses/cs348b-01/course8.pdf
 // RUSSIAN ROULETTE___________________________________________
 //___________________________________________________________
 // The following will decide if a photon will be transmitted or absorbed
-// Probability for reflection
 
+// Probability for reflection
 f@Pr = max(@diffuse.x + @specular.x, @diffuse.y + @specular.y, @diffuse.z + @specular.z);
+
 // Probability of absorbtion
 f@Pa = 1 - @Pr;
+
 // Probability for diffuse reflection
 f@Pd = ((@diffuse.x + @diffuse.y + @diffuse.z) / (@diffuse.x + @diffuse.y + @diffuse.z + @specular.x + @specular.y + @specular.z)) * @Pr;
+
 //Probability for specular reflection
 f@Ps = @Pr - @Pd;
 
